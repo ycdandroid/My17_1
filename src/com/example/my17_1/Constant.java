28dde68b0c154fcaffb4202784f071fd2cf42d6b
yinchuandong <yincd520@sina.com>
2013-08-25 22:46:17 +0800

    android箱子碰撞

---
+++ src/com/example/my17_1/Constant.java
@@ -0,0 +1,15 @@
+package com.example.my17_1;
+
+public class Constant {
+	final static float UNIT_SIZE = 0.5f;
+	final static float TIME_STEP = 1.0f/60;
+	final static int MAX_SUB_STEPS=5;//最大的子步数
+    
+    final static float EYE_X=-3;//观察者的位置x
+    final static float EYE_Y=4;//观察者的位置y
+    final static float EYE_Z=3;//观察者的位置z
+    
+    final static float TARGET_X=1;//目标的位置x
+    final static float TARGET_Y=1;//目的位置Y
+    final static float TARGET_Z=-3;//目标的位置Z
+}
